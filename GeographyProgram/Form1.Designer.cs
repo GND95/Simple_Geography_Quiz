13f4ab1a34428c9e91ae7813194dfe4940c75d48
Garrett <Garrettdeblois13@yahoo.com>
2017-05-09 23:19:19 -0500

    Add project files.

---
+++ GeographyProgram/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace GeographyProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 125);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Next Question";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 139);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(216, 41);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.l_MouseUp);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(234, 139);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(218, 41);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.radioButton2_MouseUp);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 223);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(218, 41);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.radioButton3_MouseUp);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(236, 223);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(218, 41);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.radioButton4_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 37);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 37);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(592, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 37);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Percent Correct:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(454, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 37);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(775, 212);
+            this.button2.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 52);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Re-enable Answers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 268);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.Name = "Form1";
+            this.Text = "Geography Quiz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GeographyProgram/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeographyProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 500;
+            button1.Location = new Point(100, 75);
+            button1.Text = "Start Quiz!";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            label2.Visible = false;
+            label1.Visible = false;
+            label4.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            radioButton3.Visible = false;
+            radioButton4.Visible = false;
+        }
+        float correct = 0f; //values to be used to grade the quiz
+       
+        int counter = 0; // this is what will keep track of what question you are on so it can switch to the next one once the user presses the button
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //re-enable crossed out answers for the next question
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            radioButton4.Enabled = true;
+                    
+            //maybe try to make a right click to disable the radioButton to simulate crossing out the answer. ex: on right click, if radioButton1.Enabled = false then enable, else radioButton1.Enable = false
+            if (counter == 1 && radioButton4.Checked == true)
+            {
+                correct++;
+            }
+            if (counter == 2 && radioButton2.Checked == true)
+            {
+                correct++;
+            }
+            if (counter == 3 && radioButton4.Checked == true)
+            {
+                correct++;
+            }
+            if (counter == 4 && radioButton1.Checked == true)
+            {
+                correct++;
+            }
+            if (counter == 5 && radioButton3.Checked == true)
+            {
+                correct++;
+            }
+            if (counter == 6 && radioButton4.Checked == true)
+            {
+                correct++;
+            }
+            if (counter == 7 && radioButton2.Checked == true)
+            {
+                correct++;
+            }
+            if (counter == 8 && radioButton1.Checked == true)
+            {
+                correct++;
+            }
+
+            counter++; //increment the counter each time the button is presses so the program knows what question it needs to display
+
+            this.Width = 888;
+            button1.Location = new Point(599, 12);
+            label2.Visible = true;
+            label1.Visible = true;
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            radioButton3.Visible = true;
+            radioButton4.Visible = true;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+
+            if (counter == 1)
+            {
+                label2.Text = "How many countries are in \r\nEurope?";
+                button1.Text = "Next Question";
+                label1.Text = "Question\r\n1";
+                radioButton1.Text = "28";
+                radioButton2.Text = "51";
+                radioButton3.Text = "36";
+                radioButton4.Text = "44";                         
+            }
+
+            if (counter == 2)
+            {
+                label2.Text = "How many countries are in \r\nAsia?";
+                label1.Text = "Question\r\n2";
+                label3.Visible = true;
+                label4.Visible = true;
+                label4.Text = correct + "/8\r\nCorrect";
+                radioButton1.Text = "35";
+                radioButton2.Text = "48";
+                radioButton3.Text = "62";
+                radioButton4.Text = "54";           
+            }
+
+            if (counter == 3)
+            {
+                label2.Text = "How many countries are in \r\nAntarctica?";
+                label1.Text = "Question\r\n3";
+                label4.Text = correct + "/8\r\nCorrect";
+                radioButton1.Text = "1";
+                radioButton2.Text = "2";
+                radioButton3.Text = "6";
+                radioButton4.Text = "0";             
+            }
+            if (counter == 4)
+            {
+                label2.Text = "How many countries are in \r\nNorth America?";
+                label1.Text = "Question\r\n4";
+                label4.Text = correct + "/8\r\nCorrect";
+                radioButton1.Text = "23";
+                radioButton2.Text = "3";
+                radioButton3.Text = "17";
+                radioButton4.Text = "11";         
+            }
+            if (counter == 5)
+            {
+                label2.Text = "How many countries are in \r\nSouth America?";
+                label1.Text = "Question\r\n5";
+                label4.Text = correct + "/8\r\nCorrect";
+                radioButton1.Text = "8";
+                radioButton2.Text = "15";
+                radioButton3.Text = "12";
+                radioButton4.Text = "19";            
+            }
+            if (counter == 6)
+            {
+                label2.Text = "How many countries are in \r\nAfrica?";
+                label1.Text = "Question\r\n6";
+                label4.Text = correct + "/8\r\nCorrect";
+                radioButton1.Text = "68";
+                radioButton2.Text = "39";
+                radioButton3.Text = "27";
+                radioButton4.Text = "54";          
+            }
+            if (counter == 7)
+            {
+                label2.Text = "How many countries are in \r\nAustralia?";
+                label1.Text = "Question\r\n7";
+                label4.Text = correct + "/8\r\nCorrect";
+                radioButton1.Text = "3";
+                radioButton2.Text = "1";
+                radioButton3.Text = "5";
+                radioButton4.Text = "0";              
+            }
+            if (counter == 8)
+            {
+                label2.Text = "How many total countries\r\nare there in the world?";
+                label1.Text = "Question\r\n8";
+                radioButton1.Text = "196";
+                radioButton2.Text = "301";
+                radioButton3.Text = "155";
+                radioButton4.Text = "113";
+                label4.Text = correct + "/8\r\nCorrect";
+                button1.Text = "Click to Finish";              
+            }
+            if (counter == 9)
+            {
+                float grade = correct / (counter-1);
+                label3.Text += "\r\n" + grade * 100 + "%\r\nGrade:";
+                label4.Text = correct + "/8\r\nCorrect";
+                button1.Text = "Click to Retry";
+                if (grade > .894)
+                {
+                    label3.Text += " A";
+                }
+                else
+                {
+                    if (grade > .794)
+                    {
+                        label3.Text += " B";
+                    }
+                    else
+                    {
+                        if (grade > .694)
+                        {
+                            label3.Text += " C";
+                        }
+                        else
+                        {
+                            if (grade > .594)
+                            {
+                                label3.Text += " D";
+                            }
+                            else
+                            {
+                                if (grade < .6)
+                                {
+                                    label3.Text += " F";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        
+            if (counter == 10)
+            {
+                Application.Restart();
+            }
+        }
+
+
+
+        //section for right click disabling of answers
+        private void l_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                radioButton1.Enabled = false;
+            }
+        }
+
+        private void radioButton2_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                radioButton2.Enabled = false;
+            }
+        }
+
+        private void radioButton3_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                radioButton3.Enabled = false;
+            }
+        }
+
+        private void radioButton4_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                radioButton4.Enabled = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            radioButton4.Enabled = true;
+        }
+
+    }
+}
+
+
